c35c2c279f70f43d99d88b7c870698946fe0c6ec
karitsky <valery.karitsky@gmail.com>
2019-10-14 15:21:13 +0300

    first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+
+    // каждый проход с нуля считаем сколько осталось нерешенных ячеек
+    moreToGo = 0;
+    remaining = {};
+
+    // находим пустые ячейки
+    for (let x = 0; x < 9; x++) {
+      for (let y = 0; y < 9; y++) {
+        if (matrix[x][y] == 0) {
+
+          //какие цифры могут быть
+          let candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+          
+          //проверяем какие цифры уже есть в горизонтали
+          for (let i = 0; i < 9; i++) {
+            if (matrix[x][i] != 0) {
+              if (candidates.includes(matrix[x][i])) {
+                candidates.splice(candidates.indexOf(matrix[x][i]), 1);
+              }
+            }
+          }
+
+          //проверяем какие цифры уже есть в вертикали
+          for (let i = 0; i < 9; i++) {
+            if (matrix[i][y] != 0) {
+              if (candidates.includes(matrix[i][y])) {
+                candidates.splice(candidates.indexOf(matrix[i][y]), 1);
+              }
+            }
+          }
+
+          //проверяем какие цифры уже есть в блоке
+          for (let blockX = Math.floor(x / 3) * 3; blockX < Math.floor(x / 3) * 3 + 3; blockX++) {
+            for (let blockY = Math.floor(y / 3) * 3; blockY < Math.floor(y / 3) * 3 + 3; blockY++) {
+              if (matrix[blockX][blockY] != 0) {
+                if (candidates.includes(matrix[blockX][blockY])) {
+                  candidates.splice(candidates.indexOf(matrix[blockX][blockY]), 1);
+                }
+              }
+            }
+          }
+          
+          //если только одна цифра подойдет на это место, то вставляем
+          if (candidates.length == 1) {
+            matrix[x][y] = candidates[0];
+          } else {
+            remaining[matrix[x][y] = candidates];
+            moreToGo++;
+          }
+        }
+      }
+    }
+  return matrix;
 }
